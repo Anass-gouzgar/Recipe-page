7ba434d2e8ab505d0a70a386941ead9616d95f5d
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-05-14 23:31:07 +0100

    added the margin button and increase the padding

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ Nutrition = dynamic(() => import("./components/Nutrition"),{loading:() => <p></p
 
 export default function Home() {
   return (
-    <main className="bg-bgColor md:p-20">
+    <main className="bg-bgColor md:p-32 mb-16">
       <div className="max-w-[1000px] rounded-lg flex flex-col items-center mx-auto md:p-4 bg-white" >
         <Hero />
         <Ingredients />
